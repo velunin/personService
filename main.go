41567c15cf57530899ec92b54e0a52e602439be9
Vsevolod Elunin <seva.elunin@gmail.com>
2022-01-06 17:53:02 +0300

    cleanup

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"net/http"
 	pb "personService/api/go"
 	personcommands "personService/app/commands/persons"
 	"personService/app/config"
@@ -14,7 +13,6 @@ import (
 	"personService/app/repositories"
 	personrepo "personService/app/repositories/person"
 	"personService/app/rpc"
-	"strings"
 )
 
 const (
@@ -26,9 +24,9 @@ func main() {
 	app := fx.New(
 		fx.Provide(
 			config.SetupConfigs,
-			repositories.NewTransaction,
 
 			rpc.New,
+			repositories.NewTransaction,
 			personrepo.NewPersonRepository,
 			personqueries.NewPersonQueryService,
 			personcommands.NewPersonCommandService,
@@ -64,16 +62,4 @@ func registerHooks(
 			},
 		},
 	)
-}
-
-func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO(tamird): point to merged gRPC code rather than a PR.
-		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			otherHandler.ServeHTTP(w, r)
-		}
-	})
 }
